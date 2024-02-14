cd69487d17d38a8de88e5a0482ae0654171c8c4d
Colin Gilbert <colingilbert86@gmail.com>
2024-02-14 15:22:23 -0600

    Modernized

--- infrastructure/lib/infrastructure-stack.ts
+++ infrastructure/lib/infrastructure-stack.ts
@@ -1,68 +1,86 @@
-import { Port, SecurityGroup, SubnetType, Vpc } from '@aws-cdk/aws-ec2';
-import { AwsLogDriver, Cluster, ContainerImage, FargateService, FargateTaskDefinition } from '@aws-cdk/aws-ecs';
-import { ApplicationLoadBalancer, ApplicationProtocol, Protocol as ELBProtocol } from '@aws-cdk/aws-elasticloadbalancingv2';
-import { PrivateDnsNamespace } from '@aws-cdk/aws-servicediscovery';
-import { CfnOutput, Construct, Duration, Stack, StackProps } from '@aws-cdk/core';
-import { resolve } from 'path';
-
+import { Port, SecurityGroup, SubnetType, Vpc } from "aws-cdk-lib/aws-ec2";
+import {
+  AwsLogDriver,
+  Cluster,
+  ContainerImage,
+  FargateService,
+  FargateTaskDefinition,
+} from "aws-cdk-lib/aws-ecs";
+import {
+  ApplicationLoadBalancer,
+  ApplicationProtocol,
+  Protocol as ELBProtocol,
+} from "aws-cdk-lib/aws-elasticloadbalancingv2";
+import { PrivateDnsNamespace } from "aws-cdk-lib/aws-servicediscovery";
+import { CfnOutput, Duration, Stack, StackProps } from "aws-cdk-lib";
+import { resolve } from "path";
+import { Construct } from "constructs";
 
 export class InfrastructureStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
     // fargate clusterø
-    const vpc = Vpc.fromLookup(this, 'default-vpc', { isDefault: true });
-    const cluster = new Cluster(this, 'Cluster', {
+    const vpc = Vpc.fromLookup(this, "default-vpc", { isDefault: true });
+    const cluster = new Cluster(this, "Cluster", {
       vpc: vpc,
     });
 
-    const alb = new ApplicationLoadBalancer(this, 'public-alb', {
+    const alb = new ApplicationLoadBalancer(this, "public-alb", {
       vpc,
       internetFacing: true,
     });
-    new CfnOutput(this, 'alb-dns-name', { value: `http://${alb.loadBalancerDnsName}` });
+    new CfnOutput(this, "alb-dns-name", {
+      value: `http://${alb.loadBalancerDnsName}`,
+    });
 
-    const listener = alb.addListener('http-listener', {
+    const listener = alb.addListener("http-listener", {
       port: 80,
       protocol: ApplicationProtocol.HTTP,
     });
 
-    const namespace = new PrivateDnsNamespace(this, 'test-namespace', {
+    const namespace = new PrivateDnsNamespace(this, "test-namespace", {
       vpc,
-      name: 'service.local',
+      name: "service.local",
     });
 
-    const { service: backendFGService, privateDnsName: backendDnsName, port: backendPort } = this.createService(
-      'backend-1',
+    const {
+      service: backendFGService,
+      privateDnsName: backendDnsName,
+      port: backendPort,
+    } = this.createService(
+      "backend-1",
       cluster,
-      ContainerImage.fromAsset(resolve(__dirname, '..', '..', 'services', 'color-teller-backend')),
+      ContainerImage.fromAsset(
+        resolve(__dirname, "..", "..", "services", "color-teller-backend")
+      ),
       namespace,
       {
-        COLOR: 'red',
+        COLOR: "red",
       }
-    )
+    );
 
     const { service: clientFGService, port: appPort } = this.createService(
-      'client-1',
+      "client-1",
       cluster,
-      ContainerImage.fromAsset(resolve(__dirname, '..', '..', 'services', 'color-teller-client')),
+      ContainerImage.fromAsset(
+        resolve(__dirname, "..", "..", "services", "color-teller-client")
+      ),
       namespace,
       {
         COLOR_BACKEND: `http://${backendDnsName}:${backendPort}`,
-        VERSION: 'vanilla'
+        VERSION: "vanilla",
       }
-    )
+    );
 
-
-
-    listener.addTargets('default', {
+    listener.addTargets("default", {
       port: appPort,
       protocol: ApplicationProtocol.HTTP,
       healthCheck: {
         protocol: ELBProtocol.HTTP,
         interval: Duration.seconds(10),
         port: `${appPort}`,
-        path: '/health',
+        path: "/health",
       },
       targets: [clientFGService],
     });
@@ -75,11 +93,11 @@ export class InfrastructureStack extends Stack {
     cluster: Cluster,
     image: ContainerImage,
     namespace: PrivateDnsNamespace,
-    envOverwrite: { [key: string]: string } = {},
+    envOverwrite: { [key: string]: string } = {}
   ): {
-    service: FargateService,
-    port: number,
-    privateDnsName: string,
+    service: FargateService;
+    port: number;
+    privateDnsName: string;
   } {
     const taskDef = new FargateTaskDefinition(this, `${id}-fargate-task-def`, {
       memoryLimitMiB: 1024,
@@ -90,9 +108,9 @@ export class InfrastructureStack extends Stack {
       logging: new AwsLogDriver({ streamPrefix: `${id}-app-` }),
       portMappings: [{ containerPort: appPort }],
       environment: {
-        'PORT': `${appPort}`,
-        ...envOverwrite
-      }
+        PORT: `${appPort}`,
+        ...envOverwrite,
+      },
     });
     const service = new FargateService(this, `${id}-service`, {
       cluster,
@@ -101,10 +119,12 @@ export class InfrastructureStack extends Stack {
       desiredCount: 1,
       taskDefinition: taskDef,
       vpcSubnets: { subnetType: SubnetType.PUBLIC },
-      securityGroups: [new SecurityGroup(this, `${id}-default-sg`, {
-        securityGroupName: `${id}-fargate-service`,
-        vpc: cluster.vpc,
-      })],
+      securityGroups: [
+        new SecurityGroup(this, `${id}-default-sg`, {
+          securityGroupName: `${id}-fargate-service`,
+          vpc: cluster.vpc,
+        }),
+      ],
       cloudMapOptions: {
         name: id,
         cloudMapNamespace: namespace,
@@ -114,7 +134,7 @@ export class InfrastructureStack extends Stack {
     return {
       service,
       port: appPort,
-      privateDnsName: `${id}.${namespace.namespaceName}`
-    }
+      privateDnsName: `${id}.${namespace.namespaceName}`,
+    };
   }
 }
